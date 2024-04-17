46b909b4f9b7ecbb7eb1003040f27053c4daafd9
keremeyuboglu <32223948+keremeyuboglu@users.noreply.github.com>
2024-04-17 21:42:58 +0300

    Seed Commit

--- app.py
+++ app.py
@@ -1,9 +1,108 @@
 from flask import Flask, jsonify
-from db_util import check_if_non_seeded_db, seed_db
-import json
 from util import get_seed_data, convert_keys_to_pascal_case
-from db import db
+from db import db, Base
+from flask import Flask
+from sqlalchemy import Table, ForeignKey, Column
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Mapped, mapped_column
 
+
+def check_if_non_seeded_db():
+    with app.app_context():
+        try:
+            item = db.session.execute(db.select(MenuItem)).scalar_one()
+            return False
+        except:
+            return True
+
+def seed_db(data_dict):
+    with app.app_context():
+        menu_groups = data_dict["menu_group_map"]
+        menu_items = data_dict["menu_item_map"]
+        menu_group_mappings: dict = data_dict["menu_group_menu_item_lookup"]
+
+        for menu_group_key in menu_groups:
+            menu_group_attributes=menu_groups[menu_group_key]
+            menu_group_entity = MenuGroup(**menu_group_attributes)
+            db.session.add(menu_group_entity)
+
+        for menu_item_key in menu_items:
+            menu_item_attributes=menu_items[menu_item_key]
+            menu_item_entity = MenuItem(**menu_item_attributes)
+            db.session.add(menu_item_entity)
+
+
+        db.session.commit()
+        for menu_group_id in menu_group_mappings:
+            menu_item_ids = menu_group_mappings[menu_group_id]
+            for menu_item_id in menu_item_ids:
+                menu_map = MenuGroupItemMap(
+                    menu_group_id=menu_group_id,
+                    menu_item_id=menu_item_id
+                )
+                db.session.add(menu_map)
+                # insert_statement = menu_group_item_map_table.insert().values(
+                #     MenuGroupId=menu_group_id, MenuItemId=menu_item_id
+                # )
+                # db.session.execute(insert_statement)
+                flg = 5
+        
+        db.session.commit()
+                
+        best_flg = 5
+
+  
+
+
+# menu_group_item_map_table = Table(
+#     "MenuGroupItemMap",
+#     Base.metadata,
+#     Column("MenuGroupId", ForeignKey("MenuGroup.Id"), primary_key = True),
+#     Column("MenuItemId", ForeignKey("MenuItem.Id"), primary_key = True),
+#     extend_existing=True
+# )
+
+from typing import List
+
+class MenuItem(db.Model):
+    __tablename__ = "MenuItem"
+    
+    id: Mapped[int] = mapped_column("Id", primary_key=True, unique=True)
+    name: Mapped[str] = mapped_column("Name", unique=True)
+    description: Mapped[str] = mapped_column("Description")
+    stock_status: Mapped[str] = mapped_column("StockStatus")
+    restaurant_id: Mapped[int] = mapped_column("RestaurantId")
+    image: Mapped[str] = mapped_column("Image")
+    ranking: Mapped[int] = mapped_column("Ranking")
+    price: Mapped[float] = mapped_column("Price")
+    calorie: Mapped[float] = mapped_column("Calorie")
+    menu_group_items: Mapped[List["MenuGroup"]] = relationship(
+        "MenuGroup",
+        secondary="MenuGroupItemMap", 
+        back_populates="menu_items"
+    )
+    
+class MenuGroup(db.Model):
+    __tablename__ = "MenuGroup"
+    
+    id: Mapped[int] = mapped_column("Id", primary_key=True, unique=True)
+    name: Mapped[str] = mapped_column("Name")
+    sort_order: Mapped[int] = mapped_column("SortOrder")
+    menu_items: Mapped[List["MenuItem"]] = relationship(
+        "MenuItem",
+        secondary="MenuGroupItemMap", 
+        back_populates="menu_group_items"
+    )
+
+    
+class MenuGroupItemMap(db.Model):
+    __tablename__ = "MenuGroupItemMap"
+    __table_args__ = {'extend_existing': True}
+    
+    menu_group_id: Mapped[int] = mapped_column("MenuGroupId", ForeignKey("MenuGroup.Id"), primary_key=True, unique=True)
+    menu_item_id: Mapped[int] = mapped_column("MenuItemId", ForeignKey("MenuItem.Id"), primary_key=True, unique=True)
 
 
 database_credentials = {
@@ -22,7 +121,7 @@ db.init_app(app)
 
 if check_if_non_seeded_db():
     seed_data = get_seed_data()
-    seed_data = convert_keys_to_pascal_case(seed_data)
+    # seed_data = convert_keys_to_pascal_case(seed_data)
     seed_db(seed_data)
 
 
