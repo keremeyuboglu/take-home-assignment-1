7fca68212e7ffc7fd911d905282937387585c785
keremeyuboglu <32223948+keremeyuboglu@users.noreply.github.com>
2024-04-20 22:19:50 +0300

    sda

--- create_db.py
+++
@@ -1,9 +0,0 @@
-from app.database import db
-from app import create_app
-
-app = create_app()
-with app.app_context() as ctx:
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
-
--- test_app.py
+++
@@ -1,8 +0,0 @@
-from dotenv import dotenv_values,load_dotenv 
-import json
-import os
-
-load_dotenv()
-config = dotenv_values(".env")  # config = {"USER": "foo", "EMAIL": "foo@example.org"}
-print(json.dumps(config, indent=4))
-print(os.environ)
